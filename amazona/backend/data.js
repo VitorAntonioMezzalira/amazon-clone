b1b62bb9552dc8fa95b52ba81316959034d4832c
VitorAntonioMezzalira <62727984+VitorAntonioMezzalira@users.noreply.github.com>
2020-11-05 23:15:36 -0300

    First Commit after a long time

---
+++ amazona/backend/data.js
@@ -0,0 +1,46 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.webp',
+            price: 60,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 5
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.webp',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.webp',
+            price: 50,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 15
+        },
+        {
+            _id: '4',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.webp',
+            price: 90,
+            brand: 'Nike',
+            rating: 2.5,
+            numReviews: 20
+        },
+    ]
+}
+
+export default data;
