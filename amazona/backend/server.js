2edd6ec3cbfb4ac90143e44fab33182c3c2ae72b
VitorAntonioMezzalira <62727984+VitorAntonioMezzalira@users.noreply.github.com>
2020-12-01 22:55:45 -0300

    start using mongoDB

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -1,7 +1,26 @@
 import express from 'express';
-import data from './data'
+import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+
+console.log(mongodbUrl);
+
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
 
 const app = express();
+
+app.use('/api/users/', userRoute);
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
